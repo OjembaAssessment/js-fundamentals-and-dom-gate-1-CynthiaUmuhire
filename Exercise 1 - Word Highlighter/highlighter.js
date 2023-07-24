55808884fbe9d76ce260b0a8811e0f70baf88092
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-07-24 12:57:11 +0200

    highting exercise

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,42 +1,32 @@
-let paragraph = document.getElementById('myParagraph');
+let para = document.getElementById('myParagraph');
 
-function highlightWords(paragraph) {
-    // Get the words in the paragraph.
-    var words = paragraph.split(" ");
+function highlight() {
+    const texts = para.textContent;
+    const words = texts.toLowerCase().match(/\b\w+\b/g);
 
-    // Count the occurrences of each word.
-    var wordCounts = {};
-    for (var i = 0; i < words.length; i++) {
-        if (wordCounts[words[i]] === undefined) {
-            wordCounts[words[i]] = 1;
-        } else {
-            wordCounts[words[i]]++;
-        }
-    }
-
-    // Get the 5 most occurring words.
-    var mostOccuringWords = [];
-    for (var word in wordCounts) {
-        mostOccuringWords.push({
-            word: word,
-            count: wordCounts[word]
-        });
-    }
-    mostOccuringWords.sort(function (a, b) {
-        return b.count - a.count;
+    const occurrences = {};
+    words.forEach((word) => {
+        occurrences[word] = (occurrences[word] || 0) + 1;
     });
-    mostOccuringWords = mostOccuringWords.slice(0, 5);
 
-    // Highlight the 5 most occurring words.
-    for (var i = 0; i < mostOccuringWords.length; i++) {
-        var word = mostOccuringWords[i].word;
-        if (word[0] === " ") {
-            word = word.substring(1);
+    const sortedWords = Object.keys(occurrences).sort((a, b) => occurrences[b] - occurrences[a]);
+    const top5Words = sortedWords.slice(0, 5);
+
+    const highlightedText = texts.replace(/\b\w+\b/g, (word) => {
+        const isCapitalized = word[0] === word[0].toUpperCase();
+        if (top5Words.includes(word.toLowerCase()) && isCapitalized) {
+            return `<u><mark>${word}</mark></u>`;
+        } else if (top5Words.includes(word.toLowerCase())) {
+            return `<mark>${word}</mark>`;
+        } else {
+            return word;
         }
-        paragraph = paragraph.replace(word, "<u>" + word + "</u>");
-    }
+    });
 
-    // Return the highlighted paragraph.
-    return paragraph;
+    para.innerHTML = highlightedText;
 }
-console.log("Exercise 1 - Word Highlighter");
+
+highlight();
+
+
+
--- Exercise 2 - Word Cloud/cloud.js
+++
@@ -1,1 +0,0 @@
-console.log("Exercise 2 - Word Cloud");
